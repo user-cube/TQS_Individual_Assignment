676a9cd2c76a1c0b91c95de90719660938c7f5f7
Rui Coelho <ruicoelho@ua.pt>
2020-04-04 17:45:03 +0100

    code style

--- src/main/java/pt/ua/tqs/airquality/controllers/CityController.java
+++ src/main/java/pt/ua/tqs/airquality/controllers/CityController.java
@@ -14,7 +14,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pt.ua.tqs.airquality.entities.CitiesCoordinates;
 import pt.ua.tqs.airquality.entities.CitiesNames;
+import pt.ua.tqs.airquality.services.BreezometerService;
 import pt.ua.tqs.airquality.services.CacheService;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import java.util.*;
 
@@ -49,14 +52,24 @@ public class CityController {
     @GetMapping(value="/{city}")
     public ResponseEntity getAirConditions(@PathVariable("city") String city){
         String cityName = city.toUpperCase().replaceAll("\\s", "");
-        String latitude = CitiesCoordinates.valueOf(cityName + "_LAT").toString();
-        String longitude = CitiesCoordinates.valueOf(cityName + "_LONG").toString();
+        String latitude = "";
+        String longitude = "";
+
+        try {
+            latitude = CitiesCoordinates.valueOf(cityName + "_LAT").toString();
+            longitude = CitiesCoordinates.valueOf(cityName + "_LONG").toString();
+        } catch (Exception exception){
+            Logger.getLogger(BreezometerService.class.getName()).log(Level.SEVERE, null, exception);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body("No content.");
+        }
+
         JSONObject json = null;
         String data = cacheService.getAirConditions(latitude, longitude, city);
         try {
             json = (JSONObject) new JSONParser().parse(data);
-        } catch (ParseException e) {
-            e.printStackTrace();
+        } catch (ParseException exception) {
+            Logger.getLogger(BreezometerService.class.getName()).log(Level.SEVERE, null, exception);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Something went wrong.");
         }
         return ResponseEntity.status(HttpStatus.OK).body(json);
     }
